6ca3d32271fc7dd66f0057cdde3a50f2d1ebc058
santoshkitkare <santoshkitkare@gmail.com>
2025-11-05 11:48:49 +0530

    Remove commented code

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -1,27 +1,3 @@
-# import json, smtplib, os
-# from email.message import EmailMessage
-
-# def handler(event, context):
-#     data = json.loads(event['body'])
-#     name, email, message = data['name'], data['email'], data['message']
-
-#     msg = EmailMessage()
-#     msg.set_content(f"Message from {name} ({email}):\n\n{message}")
-#     msg['Subject'] = 'New Portfolio Contact'
-#     msg['From'] = os.environ['SMTP_USER']
-#     msg['To'] = os.environ['TARGET_EMAIL']
-
-#     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-#         smtp.login(os.environ['SMTP_USER'], os.environ['SMTP_PASS'])
-#         smtp.send_message(msg)
-
-#     return {
-#         'statusCode': 200,
-#         'headers': {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-#         'body': json.dumps({'message': 'Email sent successfully'})
-#     }
-
-
 import json, os, smtplib
 from email.message import EmailMessage
 
